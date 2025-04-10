8dae21c7663d55c2e9163106fe6feaf5e87f2d22
Pontax02 <ppontanilla07@gmail.com>
2025-04-10 14:18:32 +0200

    :sparkles: feat(Golfplay): Añadir subclases computecard y strokeplay que implementan la interfaz Golfplay

---
+++ src/main/java/edu/estatuas/ComputeCard.java
@@ -0,0 +1,12 @@
+package edu.estatuas;
+
+public class ComputeCard implements GolfPlay{
+
+
+
+
+    @Override
+    public byte implementStrategy(){
+        return 0;
+    }
+}
---
+++ src/main/java/edu/estatuas/StrokePlay.java
@@ -0,0 +1,13 @@
+package edu.estatuas;
+
+public class StrokePlay implements GolfPlay {
+
+
+
+
+    @Override
+    public byte implementStrategy(){
+        return 0;
+    }
+
+}
