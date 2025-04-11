c615097cb465c0ac9655648fe3e6a6c324936ee4
Pontax02 <ppontanilla07@gmail.com>
2025-04-11 13:54:13 +0200

    :sparkles: feat(ScorecardTest): Añadir un metodo beforeclass

--- src/test/java/edu/estatuas/ScoreCardTest.java
+++ src/test/java/edu/estatuas/ScoreCardTest.java
@@ -8,18 +8,18 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
+
+
 public class ScoreCardTest {
+
+    private static Player playerA;
+    private static Player playerB;
+    private static Player playerC;
+    private static Player playerD;
+    private static ScoreCard scoreCard;
 
     @BeforeClass
     public static void ScoreSetup() {
-
-    }
-
-
-    @Test
-    public void testScoreCard() {
-
-
         Player playerA = new Player("MJ", (byte) 0);
         Player playerB = new Player("PM", (byte) 0);
         Player playerC = new Player("MM", (byte) 0);
@@ -29,11 +29,19 @@ public class ScoreCardTest {
         scoreCard.setPlayerB(playerB);
         scoreCard.setPlayerC(playerC);
         scoreCard.setPlayerD(playerD);
+
+    }
+
+
+    @Test
+    public void testScoreCard() {
+
         assertEquals(Optional.of(playerA),scoreCard.getPlayerA());
         assertEquals(Optional.of(playerB),scoreCard.getPlayerB());
         assertEquals(Optional.of(playerC),scoreCard.getPlayerC());
         assertEquals(Optional.of(playerD),scoreCard.getPlayerD());
 
     }
-
+    @Test
+    public void testScoreCardInitials() {}
 }
