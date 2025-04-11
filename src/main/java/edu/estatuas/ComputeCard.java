7194e60bf161cee73e3b3828b4a6bee936e647c2
Pontax02 <ppontanilla07@gmail.com>
2025-04-11 13:28:21 +0200

    :sparkles: feat(ComputeCard): Añadir metodo compute a la clase

--- src/main/java/edu/estatuas/ComputeCard.java
+++ src/main/java/edu/estatuas/ComputeCard.java
@@ -1,12 +1,18 @@
 package edu.estatuas;
 
-public class ComputeCard implements GolfPlay{
+public class ComputeCard {
+
+    private final GolfPlay play;
+    private final ScoreCard scoreCard;
+
+    public ComputeCard(GolfPlay play, ScoreCard scoreCard) {
+        this.play = play;
+        this.scoreCard = scoreCard;
+    }
 
 
 
-
-    @Override
-    public byte implementStrategy(){
-        return 0;
+    public void compute() {
+        this.play.implementStrategy(this.scoreCard);
     }
 }
